18ea25e574c98513b0468cf52c820a23cca08c00
Thomas Evans <tevans1000@googlemail.com>
2016-07-11 15:36:25 +0100

    master template uses BootStrap

--- templates_c/287ee235addc6235e4028767b1cc55b226237425_0.file.master.tpl.php
+++ templates_c/287ee235addc6235e4028767b1cc55b226237425_0.file.master.tpl.php
@@ -1,17 +1,17 @@
 <?php
-/* Smarty version 3.1.28, created on 2016-07-11 16:23:59
+/* Smarty version 3.1.28, created on 2016-07-11 16:34:26
   from "C:\wamp\www\thesis\manuscripts\templates\master\master.tpl" */
 
 if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
   'has_nocache_code' => false,
   'version' => '3.1.28',
-  'unifunc' => 'content_5783abffbf2812_75963536',
+  'unifunc' => 'content_5783ae72a2cd11_63326038',
   'file_dependency' => 
   array (
     '287ee235addc6235e4028767b1cc55b226237425' => 
     array (
       0 => 'C:\\wamp\\www\\thesis\\manuscripts\\templates\\master\\master.tpl',
-      1 => 1468246793,
+      1 => 1468247663,
       2 => 'file',
     ),
   ),
@@ -19,12 +19,21 @@ if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl,
   array (
   ),
 ),false)) {
-function content_5783abffbf2812_75963536 ($_smarty_tpl) {
+function content_5783ae72a2cd11_63326038 ($_smarty_tpl) {
 ?>
 <!DOCTYPE html>
 <html>
 <head lang='en'>
     <meta charset='utf-8'>
+    
+    <!-- Include BootStrap -->
+    <link rel='stylesheet' href='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css'>
+    <?php echo '<script'; ?>
+ src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js'><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ src='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js'><?php echo '</script'; ?>
+>
 </head>
 <body>
     <header>
