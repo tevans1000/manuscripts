e027cda78c5c12099a98da2645db200fe3fb0543
Thomas Evans <tevans1000@googlemail.com>
2016-07-11 20:54:54 +0100

    code commented and slightly refactored to aid readability

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
 
 require 'templates/smarty-3.1.28/libs/Smarty.class.php';
-include '../get-manuscripts.php';
 
 $smarty = new Smarty();
 
+// assign variables expected by master.tpl
 $smarty->assign('header','master/header.html');
 $smarty->assign('footer','master/footer.html');
 $smarty->assign('content','homepage.tpl');
+// assign variables expected by homepage.tpl
+include '../get-manuscripts.php';
 $smarty -> assign('result',$result);
 
 $smarty->display('master/master.tpl');
